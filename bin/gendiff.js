4da1fb316a292e92e06de5452f9f5835c387d127
gr8arty <gr8arty@gmail.com>
2021-02-17 17:49:55 +0600

    feat: add executional script in bin/

---
+++ bin/gendiff.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../src/index.js';
+
+program
+  .version('0.0.1')
+  .arguments('[filepath1 [filepath2]')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    try {
+      try {
+        if (!filepath1 && filepath2) {
+          throw new Error('Specify the first argument!');
+        } if (!filepath2 && filepath1) {
+          throw new Error('Specify the second argument!');
+        } if (!filepath1 && !filepath2) {
+          throw new Error('Specify two valid files of .json or .yaml type as arguments.');
+        }
+      } catch (e) {
+        console.error(e.message);
+      } finally {
+        const path1 = path.resolve('.', filepath1);
+        const path2 = path.resolve('.', filepath2);
+        const fileData1 = fs.readFileSync(path1, 'utf-8');
+        const fileData2 = fs.readFileSync(path2, 'utf-8');
+        const diff = genDiff(fileData1, fileData2);
+        console.log(diff);
+      }
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.help || options.h) program.help();
+if (options.version || options.V) program.version();
+if (options.format || options.f) program.help();
