86ac793a53673457f53d11ddc26609ea9e0ead1b
gr8arty <gr8arty@gmail.com>
2021-02-25 15:30:12 +0600

    test: update tests
    add tests for Yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,20 +9,41 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const file1 = readFile('file1.json');
-const file2 = readFile('file2.json');
+const jsonFile1 = '../file1.json';
+const jsonFile2 = '../file2.json';
+const yamlFile1 = '../file1.yaml';
+const yamlFile2 = '../file2.yaml';
+
 const result1 = readFile('expected');
 const result2 = readFile('notExpected');
 const result3 = '{ }';
 
-describe('get the differnce between two JSON files', () => {
+describe('get the differnce between JSON files', () => {
   test('compare equality', () => {
-    expect(gendiff(file1, file2)).toMatch(result1);
+    expect(gendiff(jsonFile1, jsonFile2)).toMatch(result1);
   });
   test('compare inequality', () => {
-    expect(gendiff(file1, file2)).not.toMatch(result2);
+    expect(gendiff(jsonFile1, jsonFile2)).not.toMatch(result2);
   });
-  test('compare inequality test2', () => {
-    expect(gendiff(file1, file2)).not.toMatch(result3);
+  test('compare inequality #2', () => {
+    expect(gendiff(jsonFile1, jsonFile2)).not.toMatch(result3);
+  });
+});
+
+describe('get the differnce between Yaml files', () => {
+  test('compare equality', () => {
+    expect(gendiff(yamlFile1, yamlFile2)).toMatch(result1);
+  });
+  test('compare inequality', () => {
+    expect(gendiff(yamlFile1, yamlFile2)).not.toMatch(result2);
+  });
+  test('compare inequality #2', () => {
+    expect(gendiff(yamlFile1, yamlFile2)).not.toMatch(result3);
+  });
+});
+
+describe('get the differnce between JSON and Yaml files', () => {
+  test('compare equality between JSON and Yaml', () => {
+    expect(gendiff(yamlFile1, jsonFile2)).toMatch(result1);
   });
 });
